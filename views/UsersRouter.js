e5b6b1b5b774ccf51b127171bab0b0749b16159f
RafaOrti <rafaeloriolorticarrio@gmail.com>
2022-02-25 20:40:43 +0100

    Creación de los archivos OrdersCOntrollers.js PeliculasCOntroller.js y UsuariosCOntroller.js

--- views/UsersRouter.js
+++ views/UsersRouter.js
@@ -4,20 +4,20 @@ const router = express.Router();
 const UsuarioController = require('../controllers/UsuarioController');
 
 //CRUD RESTful
-
 //Leer todos los usuarios
-router.get('/', UsuarioController.traeUsuarios);
+router.get('/', auth,  UsuarioController.traeUsuarios);
 //http://localhost:3000/usuarios
-
-router.get('/email/:email', UsuarioController.traerUsuarioEmail);
-
-router.get('/:id', UsuarioController.traerUsuarioId);
-
-
+router.get('/email/:email', auth, UsuarioController.traerUsuarioEmail);
+router.get('/:id', auth, UsuarioController.traerUsuarioId);
 //Registro
 router.post('/', UsuarioController.registraUsuario);
 //http://localhost:3000/usuarios
-
+//Modificar datos de un Usuario
+router.put('/:id', auth, UsuarioController.updateProfile);
+//Borramos a todos los usuarios
+router.delete('/', auth, UsuarioController.deleteAll);
+//Borramos a todos los usuarios
+router.delete('/:id', auth, UsuarioController.deleteById);
 //Login
 router.post('/login', UsuarioController.logUsuario);
 //https://localhost:3000/usuarios/login
